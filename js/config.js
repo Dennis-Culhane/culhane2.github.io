b97dc61e1f2fab817310062bbca8d20cf4b69887
Dennis-Culhane <zachary-xie@foxmail.com>
2024-12-05 01:02:32 -0500

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // GitHub 配置
 const GITHUB_CONFIG = {
-    TOKEN: '1',
+    TOKEN: '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',
     REPO_OWNER: 'Dennis-Culhane',
     REPO_NAME: 'culhane2.github.io',
     BRANCH: 'main'
@@ -9,3 +9,10 @@ const GITHUB_CONFIG = {
 // GitHub API URLs
 const GITHUB_API_URL = `https://api.github.com/repos/${GITHUB_CONFIG.REPO_OWNER}/${GITHUB_CONFIG.REPO_NAME}`;
 const GITHUB_RAW_URL = `https://raw.githubusercontent.com/${GITHUB_CONFIG.REPO_OWNER}/${GITHUB_CONFIG.REPO_NAME}/${GITHUB_CONFIG.BRANCH}`;
+
+// 导出配置
+module.exports = {
+    GITHUB_CONFIG,
+    GITHUB_API_URL,
+    GITHUB_RAW_URL
+};
