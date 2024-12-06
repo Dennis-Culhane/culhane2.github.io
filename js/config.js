6273043c409fc67cfd9f6b88ac8b4e0e75f00490
Dennis-Culhane <zachary-xie@foxmail.com>
2024-12-06 09:55:22 -0500

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -5,17 +5,11 @@ window.GITHUB_CONFIG = {
     BRANCH: 'main'
 };
 
-// Function to set GitHub token securely
-window.setGitHubToken = function(token) {
-    // Store token in sessionStorage (will be cleared when browser is closed)
-    sessionStorage.setItem('github_token', token);
-};
-
-// Function to get GitHub token
-window.getGitHubToken = function() {
-    return sessionStorage.getItem('github_token');
-};
-
 // GitHub API URLs
 window.GITHUB_API_URL = `https://api.github.com/repos/${window.GITHUB_CONFIG.REPO_OWNER}/${window.GITHUB_CONFIG.REPO_NAME}`;
 window.GITHUB_RAW_URL = `https://raw.githubusercontent.com/${window.GITHUB_CONFIG.REPO_OWNER}/${window.GITHUB_CONFIG.REPO_NAME}/${window.GITHUB_CONFIG.BRANCH}`;
+
+// Helper function to get GitHub token
+window.getGitHubToken = function() {
+    return sessionStorage.getItem('github_token');
+};
